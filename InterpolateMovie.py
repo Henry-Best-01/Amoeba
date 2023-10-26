c0eda83c3d3412581731f9626fb9316d804253ca
Henry-Best-01 <51093468+Henry-Best-01@users.noreply.github.com>
2023-10-26 11:49:46 -0400

    Add files via upload
    Made interpolator into function

--- InterpolateMovie.py
+++ InterpolateMovie.py
@@ -1,95 +1,99 @@
-'''
-This script aims to take a series of snapshots and resample them at different
-time intervals to facilitate not recreating the movies every time.
-'''
-import numpy as np
-from astropy.io import fits
-import scipy
-import json
-import matplotlib.pyplot as plt
 
-input1 = "MyMovie.fits"                 # Movie stored as fits file
-input2 = "New_timestamps.json"          # New sample times to interpolate to
+def InterpolateMovie(Movie_file, New_timestamps_file, verbose=True, plot=True):
 
-verbose = True
-plot = True
+    '''
+    This function aims to take a series of snapshots and resample them at different time intervals.
 
+    Movie_file is expected to be a FITS file with primary HDU as an array of (time, space_x, space_y),
+    and an image HDU of timestamps representing the time values of each snapshot. The image HDU must be
+    the final image HDU in the HDUList.
+    New_timestamps_file is either a list or a JSON file path containing keyword "New_times" corresponding
+    to a list of new time stamps using the same units as the timestamps in the original movie.
 
-# original movie file was assumed to be f[0].data = movie, f[1].data = disk_LC (movie collapsed on spacial axes),
-# f[2].data = intrinsic X-ray signal which generated the movie, and f[3].data = snapshot timestamps
-
-with fits.open(input1) as f:
-    movie = f[0].data               # movie should have time on axis 0, while axis 1 and 2 are spacial
-    signal_points = f[-1].data       # 1d array or list
+    verbose allows some information to be printed to standard output.
+    plot creates two plots, one with the original light curve plotted with the new interpolated light curve,
+    and one where the second frame of each image is plotted side by side.
+    '''
 
 
-with open(input2) as f:             # assumes json_input_file has ['New_times'] as the list of new (hourly) timestamps
-    json_input_file = json.load(f)
-new_timestamps = json_input_file['New_times']
-if verbose:
-    print("Data will be interpolated to",len(new_timestamps), "points")
+    import numpy as np
+    from astropy.io import fits
+    import scipy
+    import json
+    import matplotlib.pyplot as plt
 
 
-initial_shape = np.shape(movie)
-if verbose: print("The initial movie shape was",initial_shape)
-npix = initial_shape[1] * initial_shape[2]
-if verbose: print("This had",npix,"pixels")
+    with fits.open(Movie_file) as f:
+        movie = f[0].data               # movie should have time on axis 0, while axis 1 and 2 are spacial
+        signal_points = f[-1].data       # 1d array or list
 
-space_positions = np.linspace(1, npix, npix)                        # Define a 2d pixel-time space
-time_positions = signal_points
-T, S = np.meshgrid(time_positions, space_positions, indexing='ij')
+    initial_shape = np.shape(movie)
+    if verbose: print("The initial movie shape was",initial_shape)
+    npix = initial_shape[1] * initial_shape[2]
+    if verbose: print("This had",npix,"pixels")
 
-intermediate_movie = np.reshape(movie, (initial_shape[0], npix))    # reshape image to a line
+    # assumes json_input_file has ['New_times'] as the list of new timestamps if New_timestamps_file is a string
+    if type(New_timestamps_file) == str:
+        with open(New_timestamps_file) as f:
+            json_input_file = json.load(f)
+        new_timestamps = json_input_file['New_times']
+    # otherwise, we assume a list or array of new timestamps are provided
+    else: new_timestamps = New_Timestamps_file
+    if verbose: print("Data will be interpolated to",len(new_timestamps), "frames")
 
-interpolation = scipy.interpolate.RegularGridInterpolator((time_positions, space_positions),
-                                        intermediate_movie, bounds_error=False, fill_value=None)
+    space_positions = np.linspace(1, npix, npix)                        # Define a 2d pixel-time space
+    time_positions = signal_points
+    T, S = np.meshgrid(time_positions, space_positions, indexing='ij')
 
-new_timelength = np.size(new_timestamps)
-new_points_t, new_points_s = np.meshgrid(new_timestamps, space_positions, indexing='ij')
-movie_resampled = interpolation((new_points_t, new_points_s))
-if verbose: print("The new interpolated movie is now shape",np.shape(movie_resampled),"in time and spacial coordinates")
+    intermediate_movie = np.reshape(movie, (initial_shape[0], npix))    # reshape image to a line
 
+    interpolation = scipy.interpolate.RegularGridInterpolator((time_positions, space_positions),
+                                            intermediate_movie, bounds_error=False, fill_value=None)
 
-# unpack to movie shape (time axis is and was first)
-resampled_movie = np.reshape(movie_resampled, (new_timelength, initial_shape[1], initial_shape[2])) 
-if verbose: print("The reconstructed movie is now shape",np.shape(resampled_movie))
+    new_timelength = np.size(new_timestamps)
+    new_points_t, new_points_s = np.meshgrid(new_timestamps, space_positions, indexing='ij')
+    movie_resampled = interpolation((new_points_t, new_points_s))
+    if verbose: print("The new interpolated movie is now shape",np.shape(movie_resampled),"in time and spacial coordinates")
 
+    # unpack to movie shape (time axis is and was first)
+    resampled_movie = np.reshape(movie_resampled, (new_timelength, initial_shape[1], initial_shape[2])) 
+    if verbose: print("The reconstructed movie is now shape",np.shape(resampled_movie))
 
-if plot:
-    fig, ax = plt.subplots()
+    if plot:
+        fig, ax = plt.subplots()
 
-    ax.plot(np.asarray(time_positions)/24, np.sum(movie, axis=(1,2)), linewidth=2)
-    ax.plot(np.asarray(new_timestamps)/24, np.sum(resampled_movie, axis=(1,2)), '-o',alpha=0.5)
+        ax.plot(np.asarray(time_positions)/24, np.sum(movie, axis=(1,2)), linewidth=2)
+        ax.plot(np.asarray(new_timestamps)/24, np.sum(resampled_movie, axis=(1,2)), '-o',alpha=0.5)
 
-    ax.set_xlabel("Time [days]")
-    ax.set_ylabel("Flux [arb]")
-    
-    fig2, ax2 = plt.subplots(1,2)
+        ax.set_xlabel("Time [days]")
+        ax.set_ylabel("Flux [arb]")
+        
+        fig2, ax2 = plt.subplots(1,2)
 
-    conts1 = ax2[0].contourf(movie[1])
-    conts2 = ax2[1].contourf(resampled_movie[1])
+        conts1 = ax2[0].contourf(movie[1])
+        conts2 = ax2[1].contourf(resampled_movie[1])
 
-    ax2[0].set_xlabel("x [px]")
-    ax2[1].set_xlabel("x [px]")
-    ax2[0].set_ylabel("y [px]")
-    ax2[1].set_ylabel("y [px]")
-    ax2[0].set_title("Orig. movie, frame 2")
-    ax2[1].set_title("Interp. movie, frame 2")
+        ax2[0].set_xlabel("x [px]")
+        ax2[1].set_xlabel("x [px]")
+        ax2[0].set_ylabel("y [px]")
+        ax2[1].set_ylabel("y [px]")
+        ax2[0].set_title("Orig. movie, frame 2")
+        ax2[1].set_title("Interp. movie, frame 2")
 
-    plt.colorbar(conts1, ax=ax2[0], label="Flux [arb.]")
-    plt.colorbar(conts2, ax=ax2[1], label="Flux [arb.]")
-
-
-    for axis in ax2:
-        axis.set_xlim(4*initial_shape[1]/9, 5*initial_shape[1]/9)
-        axis.set_ylim(4*initial_shape[2]/9, 5*initial_shape[2]/9)
-        axis.set_aspect(1)
-    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1, wspace=0.3)
-    
-    plt.show()
-    
+        plt.colorbar(conts1, ax=ax2[0], label="Flux [arb.]")
+        plt.colorbar(conts2, ax=ax2[1], label="Flux [arb.]")
 
 
+        for axis in ax2:
+            axis.set_xlim(4*initial_shape[1]/9, 5*initial_shape[1]/9)
+            axis.set_ylim(4*initial_shape[2]/9, 5*initial_shape[2]/9)
+            axis.set_aspect(1)
+        plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1, wspace=0.3)
+        
+        plt.show()
+    return resampled_movie        
+
+InterpolateMovie("MyMovie.fits", "New_timestamps.json")
 
 
 
