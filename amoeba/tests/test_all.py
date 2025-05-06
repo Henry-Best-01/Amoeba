49380fb0c9d30762d6c11f9e0897e2f6903e88d5
Henry-Best-01 <henrygamemaster@aol.com>
2025-05-06 17:58:29 +0200

    working on coverage

---
+++ amoeba/tests/test_all.py
@@ -0,0 +1,15 @@
+"""This file just makes sure to import all items to check them for coverage with other tests"""
+
+from amoeba.Classes.accretion_disk import *
+from amoeba.Classes.agn import *
+from amoeba.Classes.blr import *
+from amoeba.Classes.blr_streamline import *
+from amoeba.Classes.diffuse_continuum import *
+from amoeba.Classes.flux_projection import *
+from amoeba.Classes.magnification_map import *
+from amoeba.Classes.torus import *
+
+from amoeba.Util.util import *
+from amoeba.Util.pipeline_util import *
+from amoeba.Util.util2 import *
+
